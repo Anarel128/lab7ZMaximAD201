17353a53fa0b1c2901541a34f5e90d0f6f54026a
Anarel128 <maxfaltendota2@gmail.com>
2023-06-14 16:27:54 +0300

    lab7_2

--- maglab3/settings.py
+++ maglab3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-pc@v-k^6#81-_+^ie_y#%n$vxhczo8wl1c*t&vf%1vq7*970-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
